f136456c35530dac6cda376c1fd184a47c331927
Advaith S Rao <advaith652@gmail.com>
2024-01-02 11:42:32 +0530

    Update util_modeler.py
    small code fix in utils modeler get f1 score

--- utils/util_modeler.py
+++ utils/util_modeler.py
@@ -34,7 +34,7 @@ def get_f1_score(
         float: The F1 score.
     """
 
-    return f1_score(y_true, y_pred, average='weighted')
+    return f1_score(y_true, y_pred, average=average)
 
 def get_classification_report_confusion_matrix(
     y_true: list[int],
